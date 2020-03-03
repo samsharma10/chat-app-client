cea45bcede3c555d06c704bf027fa922c3ddcfa3
Sam Sharma <samsharma@Sams-MacBook-Pro.local>
2020-03-03 03:08:49 -0800

    sockent.io logic to chat component

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -19,8 +19,17 @@ const Chat = ({location}) => {
         setName(name);
         setRoom(room);
 
-        socket.emit('join', {name,room});
+        socket.emit('join', {name,room}, () => {
+         
+        });
+
+        return () => {
+            socket.emit('disconnect');
+
+            socket.off();
+        }
     }, [ENDPOINT, location.search]);
+    
     return (
         <h1>Chat</h1>
     )
